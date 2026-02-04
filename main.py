fbb4b5ee4dd6981a2bb24380dc2453e1e4b9696b
C4el1s-08 <204335@stmichaelcollege.nl>
2026-02-04 10:06:07 +0100

    Film toegevoegd via Youtube link
    Ik heb de film toegevoegd via een youtube link, like en subscribe.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 import subprocess
 import sys
 import os
+import webbrowser
 
 # Get the absolute path of this file to find the other scripts
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -27,6 +28,7 @@ tk.Label(root, text="Select a Game", font=("Arial", 18)).pack(pady=20)
 tk.Button(root, text="2048", width=20, height=2, command=lambda: run_game("2048.py")).pack(pady=10)
 tk.Button(root, text="Pig Game", width=20, height=2, command=lambda: run_game("pig.py")).pack(pady=10)
 tk.Button(root, text="Quiz Game", width=20, height=2, command=lambda: run_game("homescreenGameMakenPo.py")).pack(pady=10)
+tk.Button(root,text="Film",width=20,height=2,command=lambda: webbrowser.open("https://youtu.be/gAEEYzls4Es")).pack(pady=10)
 tk.Button(root, text="Quit", width=20, height=2, command=root.quit).pack(pady=10)
 
 root.mainloop()
